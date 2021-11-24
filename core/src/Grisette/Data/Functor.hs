b8a6b371efd3dc13f7d79acc5884051a9d29ba6a
Sirui Lu <siruilu@uw.edu>
2021-11-23 23:11:50 -0800

    feat: mrgFmap

---
+++ core/src/Grisette/Data/Functor.hs
@@ -0,0 +1,6 @@
+module Grisette.Data.Functor where
+import Grisette.Data.Class.Mergeable
+import Grisette.Data.Class.SimpleMergeable
+
+mrgFmap :: (UnionMOp bool f, Mergeable bool b, Functor f) => (a -> b) -> f a -> f b
+mrgFmap f a = withUnionMSimpleMergeableU $ merge $ fmap f a
