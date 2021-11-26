e852e35d40fc6c4124da2df0e62e6478407c9269
Sirui Lu <siruilu@uw.edu>
2021-11-26 07:01:08 -0800

    feat: more Model features

--- symprim/src/Grisette/Data/Prim/Model.hs
+++ symprim/src/Grisette/Data/Prim/Model.hs
@@ -20,8 +20,30 @@ valueOf (Model m) sym =
   (\d -> fromDyn @t d (error $ "Bad type" ++ show (typeRep (Proxy @t)) ++ "for symbol " ++ show sym))
     <$> M.lookup sym m
 
-exceptFor :: Model -> S.HashSet Symbol -> Model
-exceptFor (Model m) s = Model (M.filterWithKey (\k _ -> S.member k s) m)
+exceptFor :: Model -> S.HashSet TermSymbol -> Model
+exceptFor (Model m) s =
+  Model $ S.foldl' (\acc (TermSymbol sym _) -> M.delete sym acc) m s
+
+restrictTo :: Model -> S.HashSet TermSymbol -> Model
+restrictTo (Model m) s =
+  Model $ S.foldl' (\acc (TermSymbol sym _) -> case M.lookup sym m of
+    Just v -> M.insert sym v acc
+    Nothing -> acc) M.empty s
+
+extendTo :: Model -> S.HashSet TermSymbol -> Model
+extendTo (Model m) s = Model $
+  S.foldl' (\acc (TermSymbol sym dv) -> case M.lookup sym acc of
+    Just _ -> acc
+    Nothing -> M.insert sym dv acc) m s
+
+exact :: Model -> S.HashSet TermSymbol -> Model
+exact m s = restrictTo (extendTo m s) s
+
+insert :: (Typeable a) => Model -> TermSymbol -> a -> Model
+insert (Model m) (TermSymbol sym df) v =
+  if dynTypeRep df == typeOf v
+    then Model $ M.insert sym (toDyn v) m
+    else error "Bad value type"
 
 evaluateTerm :: forall a. (Typeable a, Show a) => Bool -> Model -> Term a -> Term a
 evaluateTerm _ _ c@(ConcTerm _ _) = c
