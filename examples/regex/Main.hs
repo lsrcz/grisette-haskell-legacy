cc0ae741b9cf5177f1dc524a04b1e2ca0e1aad6b
Sirui Lu <siruilu@uw.edu>
2021-12-31 22:57:27 -0800

    feat: regex synth

--- examples/regex/Main.hs
+++ examples/regex/Main.hs
@@ -6,6 +6,9 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Main where
 
@@ -23,13 +26,40 @@ import Grisette.Data.Class.Mergeable
 import Grisette.Data.Class.SimpleMergeable
 import Grisette.Data.Class.UnionOp
 import Grisette.Data.Functor
-import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B
 import Grisette.Data.SymPrim
 import GHC.Generics
 import Grisette.Data.Class.ToSym
 import Grisette.Data.Class.ToCon
 import Text.Regex.PCRE
 import Data.Maybe
+import Data.MemoTrie
+import Data.Bifunctor
+import Grisette.Data.SMT.Config
+import Grisette.Data.SMT.Solving
+import Grisette.Data.Class.PrimWrapper
+import Grisette.Data.Class.SymEval
+import Grisette.Data.Prim.Model
+import Grisette.Data.Class.SymGen
+import Data.SBV (z3)
+import Control.Monad.State
+import Control.Monad.Memo as MM
+import Grisette.Data.MemoUtils
+import Data.Hashable
+
+instance HasTrie B.ByteString where
+  newtype B.ByteString :->: a = ByteStringTrie (Either () (Char, B.ByteString) :->: a)
+  trie f = ByteStringTrie (trie (f . bs))
+  untrie (ByteStringTrie t) = untrie t . debs
+  enumerate (ByteStringTrie t) = ((fmap . first) bs . enumerate) t
+
+bs :: Either () (Char, B.ByteString) -> B.ByteString
+bs = either (const B.empty) (uncurry B.cons)
+
+debs :: B.ByteString -> Either () (Char, B.ByteString)
+debs b = case B.uncons b of
+  Just p -> Right p
+  Nothing -> Left ()
 
 mrgSuspend ::
   forall m s bool x.
@@ -71,23 +101,23 @@ weaveYieldTransducer w (Right ()) (Left (InR (Yield y c1))) = mrgSuspend (Yield 
 type PattCoro = B.ByteString -> Integer -> Coroutine (Yield (UnionM Integer)) UnionM ()
 
 primPatt :: B.ByteString -> PattCoro
-primPatt pattstr str idx =
+primPatt pattstr = memo2 $ \str idx -> -- trace (show pattstr ++ " " ++ show str ++ " " ++ show idx)
   when (idx + toInteger (B.length pattstr) <= toInteger (B.length str) && pattstr == B.take (B.length pattstr) (B.drop (fromInteger idx) str)) $
     yield $ mrgReturn $ idx + toInteger (B.length pattstr)
 
 seqPatt :: PattCoro -> PattCoro -> PattCoro
-seqPatt patt1 patt2 str idx =
+seqPatt patt1 patt2 = memo2 $ \str idx ->
   weave sequentialBinder weaveYieldTransducer (patt1 str idx) $ simpleTransducer (lift >=> patt2 str)
 
 altPatt :: PattCoro -> PattCoro -> PattCoro
-altPatt patt1 patt2 str idx = patt1 str idx >>~ patt2 str idx
+altPatt patt1 patt2 = memo2 $ \str idx -> patt1 str idx >>~ patt2 str idx
 
 plusGreedyPatt :: PattCoro -> PattCoro
-plusGreedyPatt patt str idx = weave sequentialBinder weaveYieldTransducer (patt str idx) $
+plusGreedyPatt patt = memo2 $ \str idx -> weave sequentialBinder weaveYieldTransducer (patt str idx) $
   simpleTransducer $ \i -> lift i >>= \i1 -> when (i1 /= idx) (plusGreedyPatt patt str i1) >> yield i
 
 plusLazyPatt :: PattCoro -> PattCoro
-plusLazyPatt patt str idx = weave sequentialBinder weaveYieldTransducer (patt str idx) $
+plusLazyPatt patt = memo2 $ \str idx -> weave sequentialBinder weaveYieldTransducer (patt str idx) $
   simpleTransducer $ \i -> lift i >>= \i1 -> yield i >> when (i1 /= idx) (plusLazyPatt patt str i1)
 
 plusPatt :: SymBool -> PattCoro -> PattCoro
@@ -110,26 +140,78 @@ data ConcPatt
   | ConcAltPatt ConcPatt ConcPatt
   | ConcPlusPatt ConcPatt Bool
   deriving (Show, Generic)
- 
+
 data Patt
-  = PrimPatt (UnionM B.ByteString)
+  = PrimPatt B.ByteString
   | SeqPatt (UnionM Patt) (UnionM Patt)
   | AltPatt (UnionM Patt) (UnionM Patt)
   | PlusPatt (UnionM Patt) SymBool
-  deriving (Show, Generic)
+  deriving (Show, Generic, Eq)
 
 instance Mergeable (Sym Bool) Patt
+instance SymEval Model Patt
 instance ToSym ConcPatt Patt
 instance ToCon Patt ConcPatt
+instance Hashable Patt
 
 toCoroU :: UnionM Patt -> PattCoro
 toCoroU u = getSingle $ mrgFmap toCoro u
 
+toCoroMemoU :: UnionM Patt -> MM.MemoState (MemoHashMap Patt PattCoro) Patt PattCoro PattCoro
+toCoroMemoU u = case u of
+  SingleU x -> MM.memo toCoroMemo x
+  GuardU cond ifTrue ifFalse -> do
+    t <- toCoroMemoU ifTrue
+    f <- toCoroMemoU ifFalse
+    return $ mrgIf cond t f
+  _ -> error "Should not happen"
+
+toCoroMemo :: Patt -> MM.MemoState (MemoHashMap Patt PattCoro) Patt PattCoro PattCoro
+toCoroMemo (PrimPatt s) = {-trace (show s) $-} return $ primPatt s
+toCoroMemo (SeqPatt p1 p2) = do
+  p1r <- toCoroMemoU p1
+  p2r <- toCoroMemoU p2
+  return $ seqPatt p1r p2r
+toCoroMemo (AltPatt p1 p2) = do
+  p1r <- toCoroMemoU p1
+  p2r <- toCoroMemoU p2
+  return $ altPatt p1r p2r
+toCoroMemo (PlusPatt p greedy) = do
+  p1r <- toCoroMemoU p
+  return $ plusPatt greedy p1r
+
+toCoro :: Patt -> PattCoro
+toCoro x = MM.evalMemoState (toCoroMemo x) emptyMemoHashMap
+{-
 toCoro :: Patt -> PattCoro
 toCoro (PrimPatt s) = getSingle $ mrgFmap primPatt s
 toCoro (SeqPatt p1 p2) = seqPatt (toCoroU p1) (toCoroU p2)
 toCoro (AltPatt p1 p2) = altPatt (toCoroU p1) (toCoroU p2)
 toCoro (PlusPatt p greedy) = plusPatt greedy (toCoroU p)
+-}
+
+conformsFirst :: PattCoro -> B.ByteString -> B.ByteString -> SymBool
+conformsFirst patt reg str =
+  let
+    rp = matchFirstImpl patt str
+    rc = bimap toInteger toInteger <$> listToMaybe (getAllMatches (str =~ reg) :: [(Int, Int)])
+    rc1 = MaybeT $ mrgReturn rc
+  in
+    rp ==~ rc1
+
+synthesisRegexCompiled :: GrisetteSMTConfig b -> UnionM Patt -> PattCoro -> B.ByteString -> [B.ByteString] -> IO (Maybe ConcPatt)
+synthesisRegexCompiled config patt coro reg strs =
+  let
+    constraints = conformsFirst coro reg <$> strs
+    constraint = foldr (&&~) (conc True) constraints
+  in do
+    solveRes <- solveWith config constraint
+    case solveRes of
+      Left _ -> return Nothing
+      Right mo -> return $ Just $ symevalToCon mo patt
+
+synthesisRegex :: GrisetteSMTConfig b -> UnionM Patt -> B.ByteString -> [B.ByteString] -> IO (Maybe ConcPatt)
+synthesisRegex config patt = synthesisRegexCompiled config patt (toCoroU patt)
 
 test1 = toCoro $ toSym $ ConcPrimPatt "a"
 
@@ -158,6 +240,8 @@ reg5 = "(a|b)+?"
 
 test6 = toCoro $ toSym $ ConcSeqPatt (ConcPlusPatt (ConcAltPatt (ConcPrimPatt "a") (ConcPrimPatt "b")) False) (ConcPrimPatt "c")
 
+test7 = toCoro $ toSym $ ConcSeqPatt (ConcPlusPatt (ConcAltPatt (ConcPrimPatt "") (ConcPrimPatt "a")) False) (ConcPrimPatt "c")
+
 reg6 :: B.ByteString
 reg6 = "((a|b)+?)c"
 
@@ -182,8 +266,55 @@ str6 = "c"
 str7 :: B.ByteString
 str7 = "c"
 
+sk1 :: UnionM Patt
+sk1 = runSymGenIndexed
+  (choose (toSym $ ConcPrimPatt "a") [toSym $ ConcPrimPatt "b"]) "a"
+
+freshPrim :: State (Int, String) (UnionM Patt)
+freshPrim = choose (toSym $ ConcPrimPatt "d") $ toSym <$> [ConcPrimPatt "c", ConcPrimPatt "b", ConcPrimPatt "a", ConcPrimPatt ""]
+
+binFreshPrim :: (UnionM Patt -> UnionM Patt -> Patt) -> State (Int, String) Patt
+binFreshPrim f = do
+  f1 <- freshPrim
+  f f1 <$> freshPrim
+
+seqOrAlt :: State (Int, String) (UnionM Patt)
+seqOrAlt = do
+  s <- binFreshPrim SeqPatt
+  a <- binFreshPrim AltPatt
+  choose s [a]
+
+sks :: State (Int, String) Patt
+sks = do
+  s1 <- seqOrAlt
+  s2 <- seqOrAlt
+  f1 <- freshPrim
+  b <- genSymSimpleIndexed @SymBool ()
+  let s3 = SeqPatt s1 f1
+  let p = PlusPatt (mrgReturn s3) b
+  return $ SeqPatt s2 (mrgReturn p)
+
+sk :: Patt
+sk = runSymGenIndexed sks "a"
+
+r :: Patt
+r = toSym $ (ConcSeqPatt (ConcAltPatt (ConcPrimPatt "c") (ConcPrimPatt "d")) (ConcPlusPatt (ConcSeqPatt (ConcAltPatt (ConcPrimPatt "") (ConcPrimPatt "a")) (ConcPrimPatt "b")) True))
+
+{-
+t :: [Word8]
+t = ['a', 'b', 'c', 'd']
+-}
+
+genWords :: Int -> [Char] -> [B.ByteString]
+genWords 0 _ = [B.empty]
+genWords n alph = [B.cons ch w | w <- genWords (n - 1) alph, ch <- alph]
+
+genWordsUpTo :: Int -> [Char] -> [B.ByteString]
+genWordsUpTo n alph = concatMap (`genWords` alph) [0 .. n]
+
 main :: IO ()
 main = do
+  {-
   print $ matchFirstImpl test1 str1
   print $ listToMaybe (getAllMatches (str1 =~ reg1) :: [(Int, Int)])
   print $ matchFirstImpl test2 str1
@@ -208,3 +339,9 @@ main = do
   print $ listToMaybe (getAllMatches (str3 =~ reg5) :: [(Int, Int)])
   print $ matchFirstImpl test6 str7
   print $ listToMaybe (getAllMatches (str7 =~ reg6) :: [(Int, Int)])
+  print sk1
+  -}
+  res <- synthesisRegex (UnboundedReasoning z3) (mrgSingle sk) "[cd](a?b)+?" $ genWordsUpTo 4 "abcd"
+  print res
+  print $ matchFirstImpl (toCoro r) "cabab"
+  print $ listToMaybe (getAllMatches (("cabab" :: B.ByteString) =~ ("[cd](a?b)+?" :: B.ByteString)) :: [(Int, Int)])
