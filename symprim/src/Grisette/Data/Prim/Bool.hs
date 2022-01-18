9c62c76f473f67869bea7d6cb705cd3de4a5abf8
Sirui Lu <siruilu@uw.edu>
2022-01-18 15:24:51 -0800

    feat: rewrite rule for ite on two negations

--- symprim/src/Grisette/Data/Prim/Bool.hs
+++ symprim/src/Grisette/Data/Prim/Bool.hs
@@ -168,6 +168,8 @@ instance (SupportedPrim a) => TernaryOp ITE Bool a a a where
   partialEvalTernary _ TrueTerm ifTrue _ = ifTrue
   partialEvalTernary _ FalseTerm _ ifFalse = ifFalse
   partialEvalTernary _ _ ifTrue ifFalse | ifTrue == ifFalse = ifTrue
+  partialEvalTernary _ cond (NotTerm nifTrue) (NotTerm nifFalse) =
+    fromJust $ castTerm $ notb $ partialEvalTernary ITE cond nifTrue nifFalse
   partialEvalTernary _ (NotTerm ncond) ifTrue ifFalse = partialEvalTernary ITE ncond ifFalse ifTrue
   partialEvalTernary _ (ITETerm cc ct cf) (ITETerm tc tt tf) (ITETerm fc ft ff)
     | cc == tc && cc == fc = iteterm cc (iteterm ct tt ft) (iteterm cf tf ff)
