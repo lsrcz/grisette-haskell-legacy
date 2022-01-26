da78210b087b3ba5f9ecc63f7472662e2541743b
Sirui Lu <siruilu@cs.washington.edu>
2022-01-26 21:58:59 +0000

    feat: lifted functions

---
+++ lifted/src/Grisette/Data/List.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Grisette.Data.List where
+
+import Control.Monad.Except
+import Grisette.Data.Class.Bool
+import Grisette.Data.Class.Error
+import Grisette.Data.Class.Integer
+import Grisette.Data.Class.Mergeable
+import Grisette.Data.Class.SimpleMergeable
+import Control.Exception
+
+(!!~) ::
+  ( SymBoolOp bool,
+    SymIntegerOp bool integer,
+    UnionMOp bool uf,
+    MonadError e uf,
+    TransformError ArrayException e,
+    Mergeable bool a
+  ) =>
+  [a] ->
+  integer ->
+  uf a
+l !!~ p = go l p 0
+  where
+    go [] _ _ = throwError $ transformError (IndexOutOfBounds "!!~")
+    go (x : xs) p1 i = mrgGuard (p1 ==~ i) (mrgSingle x) (go xs p1 $ i + 1)
