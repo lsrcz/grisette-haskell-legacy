b1f7ecbfd5f9d9240e834b48ffd371b8df18d9f8
Sirui Lu <siruilu@cs.washington.edu>
2022-01-28 09:07:05 +0000

    feat: mrgFilter

--- lifted/src/Grisette/Data/List.hs
+++ lifted/src/Grisette/Data/List.hs
@@ -28,3 +28,12 @@ l !!~ p = go l p 0
   where
     go [] _ _ = throwError $ transformError (IndexOutOfBounds "!!~")
     go (x : xs) p1 i = mrgGuard (p1 ==~ i) (mrgSingle x) (go xs p1 $ i + 1)
+
+symFilter :: (SymBoolOp bool, UnionMOp bool u, Mergeable bool a, Monad u) => (a -> bool) -> [a] -> u [a]
+symFilter f = go
+  where
+    go [] = mrgSingle []
+    go (x:xs) = do
+      r <- go xs
+      mrgGuard (f x) (mrgSingle (x : r)) (mrgSingle r)
+
