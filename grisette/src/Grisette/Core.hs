31323cfae2451a45494e62cd87df97574fc518c7
Sirui Lu <siruilu@cs.washington.edu>
2022-02-10 15:23:13 +0000

    chore: export gassert

--- grisette/src/Grisette/Core.hs
+++ grisette/src/Grisette/Core.hs
@@ -16,6 +16,7 @@ module Grisette.Core
     TransformError (..),
     gthrowError,
     gassertWithError,
+    gassert,
     ExtractSymbolics (..),
     MergeStrategy (..),
     derivedMergeStrategy,
