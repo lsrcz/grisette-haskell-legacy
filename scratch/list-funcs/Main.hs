8305bdd660435ebdec9c6dd82a3a859722a9bb4c
Sirui Lu <siruilu@cs.washington.edu>
2022-03-11 22:09:39 +0000

    feat: list function test

---
+++ scratch/list-funcs/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+module Main where
+
+import Grisette.Core
+import Grisette.SymPrim.Term
+import Grisette.Lib
+
+symFilter1 :: forall bool u a. (SymBoolOp bool, MonadUnion bool u, Mergeable bool a) => (a -> bool) -> [a] -> u [a]
+symFilter1 f l = go l (mrgReturn [])
+  where
+    go :: [a] -> u [a] -> u [a]
+    go [] r = mrgFmap reverse r
+    go (x : xs) r = withUnionSimpleMergeable @bool @u @[a] $ do
+      res <- mrgIf (f x) (mrgReturn $ mrgFmap (x:) r) (mrgReturn r)
+      go xs res
+
+
+main :: IO()
+main = do
+  print $ (symFilter id [ssymb "a", ssymb "b", ssymb "c"] :: UnionM [SymBool])
+  print $ do
+    x1 <- (symFilter1 ((ssymb "f" :: Bool =~> Bool) #) [ssymb "a", ssymb "b"] :: UnionM [SymBool])
+    symFilter1 ((ssymb "f1" :: Bool =~> Bool) #) x1 :: UnionM [SymBool]
+  print $ do
+    l <- mrgIf (ssymb "c") (mrgReturn [ssymb "a"]) (mrgReturn [ssymb "a", ssymb "b"])
+    (symFilter1 id l :: UnionM [SymBool])
+  print $ do
+    l <- mrgIf (ssymb "d") (mrgIf (ssymb "e") (mrgReturn [ssymb "a"]) (mrgReturn [ssymb "a", ssymb "b"])) (mrgReturn [ssymb "a", ssymb "b", ssymb "c"])
+    (symFilter1 id l :: UnionM [SymBool])
