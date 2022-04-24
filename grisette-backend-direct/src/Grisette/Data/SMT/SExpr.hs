060e2dce09600e6803871670d9ed439730b17a80
Sirui Lu <siruilu@cs.washington.edu>
2022-04-24 21:26:19 +0000

    feat: start backend implementation

---
+++ grisette-backend-direct/src/Grisette/Data/SMT/SExpr.hs
@@ -0,0 +1,164 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Grisette.Data.SMT.SExpr
+  ( SExpr (..),
+    sexprByteString,
+    sexprByteStringBuilder,
+    symbol,
+    intSort,
+    boolSort,
+    SMTLogic (..),
+    setLogic,
+    defineFun,
+    checkSat,
+    getModel,
+    trueLit,
+    falseLit,
+    boolLit,
+    Grisette.Data.SMT.SExpr.not,
+    Grisette.Data.SMT.SExpr.and,
+    andMany,
+    Grisette.Data.SMT.SExpr.or,
+    orMany,
+  )
+where
+
+import ByteString.StrictBuilder
+-- import qualified Data.ByteString as B
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import Data.List
+
+data SExpr = Atom Builder | List [SExpr] Builder
+
+sexprByteStringBuilder :: SExpr -> Builder
+sexprByteStringBuilder (Atom v) = v
+sexprByteStringBuilder (List _ r) = r
+
+sexprByteString :: SExpr -> B.ByteString
+sexprByteString = builderBytes . sexprByteStringBuilder
+
+instance Show SExpr where
+  show = C.unpack . builderBytes . sexprByteStringBuilder
+
+space :: Builder
+space = " "
+
+{-# INLINE list #-}
+list :: [SExpr] -> SExpr
+list [] = List [] "()"
+list l@(hd:sub) =
+  List l $
+    "("
+      <> foldl' (\acc v -> acc <> space <> sexprByteStringBuilder v) (sexprByteStringBuilder hd) sub
+      <> ")"
+
+{-# INLINE apply #-}
+apply :: SExpr -> [SExpr] -> SExpr
+apply hd tl = list $ hd : tl
+
+{-# INLINE unary #-}
+unary :: SExpr -> SExpr -> SExpr
+unary op x = apply op [x]
+
+{-# INLINE binary #-}
+binary :: SExpr -> SExpr -> SExpr -> SExpr
+binary op x y = apply op [x, y]
+
+{-# INLINE ternary #-}
+ternary :: SExpr -> SExpr -> SExpr -> SExpr -> SExpr
+ternary op x y z = apply op [x, y, z]
+
+data SMTLogic = ALL | QF_BV
+
+qfbvAtom :: SExpr
+qfbvAtom = Atom "QF_BV"
+
+allAtom :: SExpr
+allAtom = Atom "ALL"
+
+logicAtom :: SMTLogic -> SExpr
+logicAtom QF_BV = qfbvAtom
+logicAtom ALL = allAtom
+
+setLogicAtom :: SExpr
+setLogicAtom = Atom "set-logic"
+
+{-# INLINE setLogic #-}
+setLogic :: SMTLogic -> SExpr
+setLogic = unary setLogicAtom . logicAtom
+
+{-# INLINE sortedSymbol #-}
+sortedSymbol :: B.ByteString -> SExpr -> SExpr
+sortedSymbol symb typ = list [symbol symb, typ]
+
+defineFunAtom :: SExpr
+defineFunAtom = Atom "define-fun"
+
+{-# INLINE defineFun #-}
+defineFun ::
+  -- | function name
+  B.ByteString ->
+  -- | function parameters with sorts
+  [(B.ByteString, SExpr)] ->
+  -- | Result sort
+  SExpr ->
+  -- | Definition
+  SExpr ->
+  SExpr
+defineFun name param resTyp def = list [defineFunAtom, symbol name, list $ uncurry sortedSymbol <$> param, resTyp, def]
+
+checkSat :: SExpr
+checkSat = list [Atom "check-sat"]
+
+getModel :: SExpr
+getModel = list [Atom "get-model"]
+
+boolSort :: SExpr
+boolSort = Atom "Bool"
+
+intSort :: SExpr
+intSort = Atom "Int"
+
+trueLit :: SExpr
+trueLit = Atom "true"
+
+falseLit :: SExpr
+falseLit = Atom "false"
+
+boolLit :: Bool -> SExpr
+boolLit True = trueLit
+boolLit False = falseLit
+
+notAtom :: SExpr
+notAtom = Atom "not"
+
+andAtom :: SExpr
+andAtom = Atom "and"
+
+orAtom :: SExpr
+orAtom = Atom "or"
+
+{-# INLINE not #-}
+not :: SExpr -> SExpr
+not = unary notAtom
+
+{-# INLINE and #-}
+and :: SExpr -> SExpr -> SExpr
+and = binary andAtom
+
+{-# INLINE andMany #-}
+andMany :: [SExpr] -> SExpr
+andMany = apply andAtom
+
+{-# INLINE or #-}
+or :: SExpr -> SExpr -> SExpr
+or = binary orAtom
+
+{-# INLINE orMany #-}
+orMany :: [SExpr] -> SExpr
+orMany = apply orAtom
+
+symbol :: B.ByteString -> SExpr
+symbol = Atom . bytes
---
+++ grisette-backend-direct/test/Grisette/Data/SMT/SExprSpec.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Grisette.Data.SMT.SExprSpec where
+
+import ByteString.StrictBuilder
+import Grisette.Data.SMT.SExpr as SExpr
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "SExpr show correct" $ do
+    it "Symbol correct" $ do
+      sexprByteString (SExpr.symbol "a") `shouldBe` "a"
+    it "Bool lit correct" $ do
+      sexprByteString (trueLit) `shouldBe` "true"
+      sexprByteString (falseLit) `shouldBe` "false"
+      sexprByteString (boolLit True) `shouldBe` "true"
+      sexprByteString (boolLit False) `shouldBe` "false"
+    it "Bool ops correct" $ do
+      sexprByteString (SExpr.not (SExpr.symbol "a")) `shouldBe` "(not a)"
+      sexprByteString (SExpr.and (SExpr.symbol "a") (SExpr.symbol "b")) `shouldBe` "(and a b)"
+      sexprByteString (SExpr.or (SExpr.symbol "a") (SExpr.symbol "b")) `shouldBe` "(or a b)"
+      sexprByteString (SExpr.andMany [SExpr.symbol "a", SExpr.symbol "b", SExpr.symbol "c"])
+        `shouldBe` "(and a b c)"
+      sexprByteString (SExpr.orMany [SExpr.symbol "a", SExpr.symbol "b", SExpr.symbol "c"])
+        `shouldBe` "(or a b c)"
+    it "sort correct" $ do
+      sexprByteString SExpr.intSort `shouldBe` "Int"
+      sexprByteString SExpr.boolSort `shouldBe` "Bool"
+    it "set-logic correct" $ do
+      sexprByteString (SExpr.setLogic QF_BV) `shouldBe` "(set-logic QF_BV)"
+      sexprByteString (SExpr.setLogic ALL) `shouldBe` "(set-logic ALL)"
+    it "define-fun correct" $ do
+      sexprByteString (SExpr.defineFun "x" [] boolSort $ SExpr.symbol "b") `shouldBe`
+        "(define-fun x () Bool b)"
+      sexprByteString (SExpr.defineFun "y" [("b", boolSort)] boolSort $ SExpr.not $ SExpr.symbol "b") `shouldBe`
+        "(define-fun y ((b Bool)) Bool (not b))"
+    it "check-sat correct" $ do
+      sexprByteString checkSat `shouldBe` "(check-sat)"
+    it "get-model correct" $ do
+      sexprByteString getModel `shouldBe` "(get-model)"
