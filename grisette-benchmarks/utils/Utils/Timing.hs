e7d762304b85c73d03286edc436a82f5c9f0f569
Sirui Lu <lsrcz@outlook.com>
2022-04-27 16:16:00 -0700

    chore: printing timespec with seconds

--- grisette-benchmarks/utils/Utils/Timing.hs
+++ grisette-benchmarks/utils/Utils/Timing.hs
@@ -2,9 +2,34 @@
 
 module Utils.Timing where
 
+import Data.Text.Lazy.Builder
 import Formatting
-import Formatting.Clock
+import Formatting.Internal
 import System.Clock
+
+fmt :: Integer -> Builder
+fmt diff
+  | Just i <- scale (10 ^ (9 :: Integer)) = bprint (fixed 2 % " s") i
+  | Just i <- scale (10 ^ (6 :: Integer)) = bprint (fixed 2 % " ms") i
+  | Just i <- scale (10 ^ (3 :: Integer)) = bprint (fixed 2 % " us") i
+  | otherwise = bprint (int % " ns") diff
+  where
+    scale :: Integer -> Maybe Double
+    scale i =
+      if diff >= i
+        then Just (fromIntegral diff / fromIntegral i)
+        else Nothing
+
+-- | Same as @durationNS@ but works on `TimeSpec` from the clock package.
+timeSpecs :: Format r (TimeSpec -> TimeSpec -> r)
+timeSpecs = Format (\g x y -> g (fmt0 x y))
+  where
+    fmt0 (TimeSpec s1 n1) (TimeSpec s2 n2) = fmt diff
+      where
+        diff :: Integer
+        diff = a2 - a1
+        a1 = (fromIntegral s1 * 10 ^ (9 :: Integer)) + fromIntegral n1
+        a2 = (fromIntegral s2 * 10 ^ (9 :: Integer)) + fromIntegral n2
 
 timeItAll :: String -> IO a -> IO a
 timeItAll str x = do
