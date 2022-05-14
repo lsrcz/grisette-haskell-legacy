6dfcda312bc96e35dd710d4f751f5aa0a2344af0
Sirui Lu <lsrcz@outlook.com>
2022-05-14 02:52:42 -0700

    doc: documents for mrg variant of Foldable and Traversable

--- grisette-core/src/Grisette/Control/Monad/Except.hs
+++ grisette-core/src/Grisette/Control/Monad/Except.hs
@@ -9,9 +9,11 @@ import Grisette.Data.Class.Bool
 import Grisette.Data.Class.Mergeable
 import Grisette.Control.Monad.Union
 
+-- | 'throwError' with 'Mergeable' knowledge propagation.
 mrgThrowError :: (SymBoolOp bool, MonadError e m, MonadUnion bool m, Mergeable bool e, Mergeable bool a) => e -> m a
 mrgThrowError = merge . throwError
 
+-- | 'catchError' with 'Mergeable' knowledge propagation.
 mrgCatchError ::
   (SymBoolOp bool, MonadError e m, MonadUnion bool m, Mergeable bool e, Mergeable bool a) =>
   m a ->
--- grisette-core/src/Grisette/Data/Traversable.hs
+++ grisette-core/src/Grisette/Data/Traversable.hs
@@ -15,6 +15,7 @@ where
 import Grisette.Control.Monad.Union
 import Grisette.Data.Class.Mergeable
 
+-- | 'traverse' with 'Mergeable' knowledge propagation.
 mrgTraverse ::
   forall bool a b t f.
   ( Mergeable bool b,
@@ -28,6 +29,7 @@ mrgTraverse ::
 mrgTraverse f = withMergeable @bool @t @b $ merge . traverse (merge . f)
 {-# INLINE mrgTraverse #-}
 
+-- | 'sequenceA' with 'Mergeable' knowledge propagation.
 mrgSequenceA ::
   forall bool a t f.
   ( Mergeable bool a,
@@ -40,6 +42,7 @@ mrgSequenceA ::
 mrgSequenceA = mrgTraverse id
 {-# INLINE mrgSequenceA #-}
 
+-- | 'mapM' with 'Mergeable' knowledge propagation.
 mrgMapM ::
   forall bool a b t f.
   ( Mergeable bool b,
@@ -53,6 +56,7 @@ mrgMapM ::
 mrgMapM = mrgTraverse
 {-# INLINE mrgMapM #-}
 
+-- | 'sequence' with 'Mergeable' knowledge propagation.
 mrgSequence ::
   forall bool a t f.
   ( Mergeable bool a,
@@ -65,6 +69,7 @@ mrgSequence ::
 mrgSequence = mrgSequenceA
 {-# INLINE mrgSequence #-}
 
+-- | 'for' with 'Mergeable' knowledge propagation.
 mrgFor ::
   ( Mergeable bool b,
     Mergeable1 bool t,
@@ -76,6 +81,7 @@ mrgFor ::
   m (t b)
 mrgFor = flip mrgTraverse
 
+-- | 'forM' with 'Mergeable' knowledge propagation.
 mrgForM ::
   ( Mergeable bool b,
     Mergeable1 bool t,
@@ -85,4 +91,4 @@ mrgForM ::
   t a ->
   (a -> m b) ->
   m (t b)
-mrgForM = flip mapM
+mrgForM = flip mrgMapM
