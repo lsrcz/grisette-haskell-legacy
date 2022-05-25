03f4e9e445a594d1ab3fa54972ef1ecd2f6f0fdc
Sirui Lu <lsrcz@outlook.com>
2022-05-25 12:50:52 -0700

    fix: fix bv orphans

--- grisette-symir/src/Grisette/Data/Prim/Orphan.hs
+++ grisette-symir/src/Grisette/Data/Prim/Orphan.hs
@@ -3,26 +3,26 @@
 {-# LANGUAGE DeriveLift #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
-module Grisette.Data.Prim.Orphan where
-import GHC.TypeNats
-import Data.Hashable
-import Data.BitVector.Sized.Signed
-import Language.Haskell.TH.Syntax
-import Control.DeepSeq
-import Data.BitVector.Sized.Unsigned
 
-instance (KnownNat w, 1 <= w) => Hashable (SignedBV w) where
+module Grisette.Data.Prim.Orphan where
+
+import Control.DeepSeq
+import Data.BitVector.Sized.Signed
+import Data.BitVector.Sized.Unsigned
+import Data.Hashable
+import Language.Haskell.TH.Syntax
+
+instance Hashable (SignedBV w) where
   s `hashWithSalt` (SignedBV b) = s `hashWithSalt` b
 
 deriving instance (Lift (SignedBV v))
 
 deriving instance (NFData (SignedBV v))
-instance (KnownNat w, 1 <= w) => Hashable (UnsignedBV w) where
+
+instance Hashable (UnsignedBV w) where
   s `hashWithSalt` (UnsignedBV b) = s `hashWithSalt` b
 
 deriving instance (Lift (UnsignedBV v))
 
 deriving instance (NFData (UnsignedBV v))
-
