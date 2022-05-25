cfcd6bdff5a562d092427a75dfefa6e288399d2e
Sirui Lu <lsrcz@outlook.com>
2022-05-25 15:32:00 -0700

    refactor: implement more in monad-coroutine

--- grisette-monad-coroutine/src/Grisette/Lib/Control/Monad/Coroutine.hs
+++ grisette-monad-coroutine/src/Grisette/Lib/Control/Monad/Coroutine.hs
@@ -1,14 +1,28 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
-{-# LANGUAGE RankNTypes #-}
 
 module Grisette.Lib.Control.Monad.Coroutine where
 
 import Control.Monad.Coroutine hiding (merge)
 import Grisette.Core
+
+liftCoroEitherMergeStrategy ::
+  (SymBoolOp bool, Mergeable1 bool s, Mergeable1 bool m) =>
+  MergeStrategy bool x ->
+  MergeStrategy bool (Either (s (Coroutine s m x)) x)
+liftCoroEitherMergeStrategy ms =
+  liftMergeStrategy2 (liftMergeStrategy (liftMergeStrategy ms)) ms
+
+coroEitherMergeStrategy ::
+  (SymBoolOp bool, Mergeable1 bool s, Mergeable1 bool m, Mergeable bool x) =>
+  MergeStrategy bool (Either (s (Coroutine s m x)) x)
+coroEitherMergeStrategy = liftMergeStrategy2 mergeStrategy1 mergeStrategy
 
 instance
   (SymBoolOp bool, Mergeable1 bool m, Mergeable bool a, Mergeable1 bool sus) =>
@@ -16,14 +30,14 @@ instance
   where
   mergeStrategy =
     wrapMergeStrategy
-      (liftMergeStrategy (liftMergeStrategy2 mergeStrategy1 mergeStrategy))
+      (liftMergeStrategy coroEitherMergeStrategy)
       Coroutine
       (\(Coroutine v) -> v)
 
 instance (SymBoolOp bool, Mergeable1 bool m, Mergeable1 bool sus) => Mergeable1 bool (Coroutine sus m) where
   liftMergeStrategy m =
     wrapMergeStrategy
-      (liftMergeStrategy (liftMergeStrategy2 (liftMergeStrategy (liftMergeStrategy m)) m))
+      (liftMergeStrategy $ liftCoroEitherMergeStrategy m)
       Coroutine
       (\(Coroutine v) -> v)
 
@@ -44,9 +58,9 @@ instance
   UnionMergeable1 bool (Coroutine sus m)
   where
   mergeWithStrategy s ((Coroutine v) :: Coroutine sus m a) =
-    Coroutine $ mergeWithStrategy (liftMergeStrategy2 (liftMergeStrategy (liftMergeStrategy s)) s) v
+    Coroutine $ mergeWithStrategy (liftCoroEitherMergeStrategy s) v
   mrgIfWithStrategy s cond (Coroutine t) (Coroutine f) =
-    Coroutine $ mrgIfWithStrategy (liftMergeStrategy2 (liftMergeStrategy (liftMergeStrategy s)) s) cond t f
+    Coroutine $ mrgIfWithStrategy (liftCoroEitherMergeStrategy s) cond t f
 
 instance
   (Monoid symbolSet, ExtractSymbolics symbolSet (m (Either (sus (Coroutine sus m a)) a))) =>
@@ -62,9 +76,29 @@ mrgSuspend ::
 mrgSuspend s =
   Coroutine $
     mergeWithStrategy
-      (liftMergeStrategy2 mergeStrategy1 mergeStrategy) $
-      return (Left s)
+      coroEitherMergeStrategy
+      $ return (Left s)
 {-# INLINEABLE mrgSuspend #-}
+
+mrgMapMonad ::
+  forall s m m' bool x.
+  (Functor s, SymBoolOp bool, Mergeable1 bool s, Mergeable bool x, Monad m, MonadUnion bool m') =>
+  (forall y. m y -> m' y) ->
+  Coroutine s m x ->
+  Coroutine s m' x
+mrgMapMonad f (Coroutine r) =
+  Coroutine
+    { resume =
+        f r >>= \x ->
+          mergeWithStrategy
+            coroEitherMergeStrategy
+            $ return $ map' x
+    }
+  where
+    map' :: Either (s (Coroutine s m x)) x -> Either (s (Coroutine s m' x)) x
+    map' (Right r1) = Right r1
+    map' (Left s) = Left $ mrgMapMonad f <$> s
+{-# INLINEABLE mrgMapMonad #-}
 
 mrgMapSuspension ::
   forall s m bool x s'.
@@ -72,11 +106,101 @@ mrgMapSuspension ::
   (forall y. s y -> s' y) ->
   Coroutine s m x ->
   Coroutine s' m x
-mrgMapSuspension f cort = Coroutine {resume = resume cort >>= \x ->
-  mergeWithStrategy (liftMergeStrategy2 mergeStrategy1 mergeStrategy) $ return $ map' x}
+mrgMapSuspension f (Coroutine r) =
+  Coroutine
+    { resume =
+        r >>= \x ->
+          mergeWithStrategy coroEitherMergeStrategy $ return $ map' x
+    }
   where
     map' :: Either (s (Coroutine s m x)) x -> Either (s' (Coroutine s' m x)) x
     map' (Right r1) = Right r1
     map' (Left s) = Left $ f $ mrgMapSuspension f <$> s
 {-# INLINEABLE mrgMapSuspension #-}
 
+mrgMapFirstSuspension ::
+  forall s m bool x.
+  (Functor s, SymBoolOp bool, Mergeable1 bool s, MonadUnion bool m, Mergeable bool x) =>
+  (forall y. s y -> s y) ->
+  Coroutine s m x ->
+  Coroutine s m x
+mrgMapFirstSuspension f (Coroutine r) =
+  Coroutine
+    { resume =
+        r >>= \s -> mrgReturnWithStrategy coroEitherMergeStrategy $
+          case s of
+            Right x -> Right x
+            Left x -> Left $ f x
+    }
+
+instance Mergeable bool (Naught x) where
+  mergeStrategy = SimpleStrategy mrgIte
+
+instance Mergeable1 bool Naught where
+  liftMergeStrategy _ = SimpleStrategy mrgIte
+
+instance SimpleMergeable bool (Naught x) where
+  mrgIte _ x _ = x
+
+instance SimpleMergeable1 bool Naught where
+  liftMrgIte _ _ x _ = x
+
+runCoroutine ::
+  (SymBoolOp bool, MonadUnion bool m, Mergeable bool x) =>
+  Coroutine Naught m x ->
+  m x
+runCoroutine (Coroutine r) = do
+  v <- r
+  case v of
+    Left _ -> error "Won't happen"
+    Right x -> mrgReturn x
+
+mrgBounce ::
+  (Functor s, SymBoolOp bool, Mergeable1 bool s, MonadUnion bool m, Mergeable bool x) =>
+  (s (Coroutine s m x) -> Coroutine s m x) ->
+  Coroutine s m x ->
+  Coroutine s m x
+mrgBounce f (Coroutine r) = Coroutine $ mergeWithStrategy coroEitherMergeStrategy $ do
+  r1 <- r
+  case r1 of
+    Left s -> resume $ f s
+    Right x -> return $ Right x
+
+mrgPogoStick ::
+  (MonadUnion bool m, Mergeable bool x) =>
+  (s (Coroutine s m x) -> Coroutine s m x) -> Coroutine s m x -> m x
+mrgPogoStick f (Coroutine r) = do
+  r1 <- r
+  case r1 of
+    Left h -> mrgPogoStick f $ f h
+    Right v -> mrgReturn v
+
+mrgPogoStickM ::
+  (MonadUnion bool m, Mergeable bool x) =>
+  (s (Coroutine s m x) -> m (Coroutine s m x)) -> Coroutine s m x -> m x
+mrgPogoStickM f (Coroutine r) = do
+  r1 <- r
+  case r1 of
+    Left h -> do
+      cs <- f h
+      mrgPogoStickM f cs
+    Right v -> mrgReturn v
+
+mrgFoldRun ::
+  (MonadUnion bool m, SymBoolOp bool, Mergeable bool x, Mergeable bool a) =>
+  (a -> s (Coroutine s m x) -> (a, Coroutine s m x)) -> a -> Coroutine s m x -> m (a, x)
+mrgFoldRun f a (Coroutine r) = do
+  r1 <- r
+  case r1 of
+    Left s -> case f a s of
+      (a1, c1) -> mrgFoldRun f a1 c1
+    Right v -> mrgReturn (a, v)
+
+type MrgPairBinder bool m =
+  forall x y r. (Mergeable bool r) => (x -> y -> m r) -> m x -> m y -> m r
+
+mrgSequentialBinder :: (SymBoolOp bool, MonadUnion bool m) => MrgPairBinder bool m
+mrgSequentialBinder f ma mb = merge $ do
+  a <- ma
+  b <- mb
+  f a b
--- grisette-monad-coroutine/src/Grisette/Lib/Control/Monad/Coroutine/SuspensionFunctors.hs
+++ grisette-monad-coroutine/src/Grisette/Lib/Control/Monad/Coroutine/SuspensionFunctors.hs
@@ -6,7 +6,6 @@ module Grisette.Lib.Control.Monad.Coroutine.SuspensionFunctors where
 import Control.Monad.Coroutine.SuspensionFunctors
 import Grisette.Core
 import Control.Monad.Coroutine
-import Data.Functor.Sum
 import Grisette.Lib.Control.Monad.Coroutine
 
 instance (SymBoolOp bool, Mergeable bool x, Mergeable bool y) => Mergeable bool (Yield x y) where
@@ -34,38 +33,10 @@ mrgYield :: (SymBoolOp bool, MonadUnion bool m, Mergeable bool x) => x -> Corout
 mrgYield x = mrgSuspend (Yield x $ mrgReturn ())
 {-# INLINEABLE mrgYield #-}
 
-simpleTransducer ::
-  (SymBoolOp bool, MonadUnion bool m, Mergeable bool a, Mergeable bool x) =>
-  (a -> Coroutine (Yield x) m ()) ->
-  Coroutine (Sum (Await a) (Yield x)) m ()
-simpleTransducer f = mrgSuspend (InL $ Await $ \x -> mapSuspension InR (f x) >> simpleTransducer f)
-{-# INLINEABLE simpleTransducer #-}
+mrgAwait :: (SymBoolOp bool, MonadUnion bool m, Mergeable bool x) => Coroutine (Await x) m x
+mrgAwait = mrgSuspend (Await mrgReturn)
+{-# INLINEABLE mrgAwait #-}
 
-mrgWeaveYieldTransducer ::
-  (SymBoolOp bool, MonadUnion bool m, Mergeable bool b) =>
-  WeaveStepper (Yield a) (Sum (Await a) (Yield b)) (Yield b) m () () ()
-mrgWeaveYieldTransducer _ _ (Right ()) = mrgReturn ()
-mrgWeaveYieldTransducer w (Left l) (Left (InR (Yield y c1))) = mrgSuspend (Yield y $ w (suspend l) c1)
-mrgWeaveYieldTransducer w (Left (Yield x c)) (Left (InL (Await f))) = w c $ f x
-mrgWeaveYieldTransducer _ (Right ()) (Left (InL (Await _))) = mrgReturn ()
-mrgWeaveYieldTransducer w (Right ()) (Left (InR (Yield y c1))) = mrgSuspend (Yield y $ w (return ()) c1)
-{-# INLINEABLE mrgWeaveYieldTransducer #-}
-
-(|->) ::
-  (SymBoolOp bool, MonadUnion bool m, Mergeable bool x) =>
-  Coroutine (Yield a) m () ->
-  Coroutine (Sum (Await a) (Yield x)) m () ->
-  Coroutine (Yield x) m ()
-(|->) = weave sequentialBinder mrgWeaveYieldTransducer
-{-# INLINEABLE (|->) #-}
-
-infixl 1 |->
-
-(|>>=) ::
-  (SymBoolOp bool, MonadUnion bool m, Mergeable bool a, Mergeable bool x) =>
-  Coroutine (Yield a) m () ->
-  (a -> Coroutine (Yield x) m ()) ->
-  Coroutine (Yield x) m ()
-(|>>=) l f = l |-> simpleTransducer f
-{-# INLINEABLE (|>>=) #-}
-
+mrgRequest :: (SymBoolOp bool, MonadUnion bool m, Mergeable bool x, Mergeable bool y) => x -> Coroutine (Request x y) m y
+mrgRequest x = mrgSuspend (Request x mrgReturn)
+{-# INLINEABLE mrgRequest #-}
