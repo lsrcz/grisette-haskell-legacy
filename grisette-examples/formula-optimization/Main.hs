383070a1caea97ee90b28d01fc0fbcf9b8421000
Sirui Lu <lsrcz@outlook.com>
2022-06-23 15:58:04 -0700

    feat: paper example formula

---
+++ grisette-examples/formula-optimization/Main.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad.Except
+import GHC.Generics
+import Grisette hiding (symAssert, symAssume)
+
+a, b, c, d, e, f :: SymBool
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+e = "e"
+f = "f"
+
+data Error = AssertError | AssumeError
+  deriving (Show, Eq, Generic)
+  deriving (Mergeable SymBool) via Default Error
+
+symAssert :: (UnionLike bool u, SymBoolOp bool, MonadError Error u) => bool -> u ()
+symAssert cond = mrgIf cond (return ()) (throwError AssertError)
+
+symAssume :: (UnionLike bool u, SymBoolOp bool, MonadError Error u) => bool -> u ()
+symAssume cond = mrgIf cond (return ()) (throwError AssumeError)
+
+-- >>> asserts
+-- ExceptT (UMrg (If (! (&& a (&& (ite b c d) e))) (Single (Left AssertError)) (Single (Right ()))))
+asserts :: ExceptT Error UnionM ()
+asserts = do
+  symAssert a
+  mrgIf b (symAssert c) (symAssert d)
+  symAssert e
+
+-- >>> mixed
+-- ExceptT (UMrg (If (! (&& a (&& (ite b c d) (&& e f)))) (If (&& a (ite (ite b c d) e b)) (Single (Left AssertError)) (Single (Left AssumeError))) (Single (Right ()))))
+mixed :: ExceptT Error UnionM ()
+mixed = do
+  symAssume a
+  mrgIf b (symAssert c) (symAssume d)
+  symAssume e
+  symAssert f
+
+data Simple = Simple
+
+instance SolverErrorTranslation Simple Error where
+  errorTranslation _ AssumeError = False
+  errorTranslation _ AssertError = True
+
+instance SymBoolOp bool => SolverTranslation Simple bool Error () where
+  valueTranslation _ _ = conc False
+
+main :: IO ()
+main = do
+  print asserts
+  print mixed
+  _ <- solveWithExcept Simple (UnboundedReasoning z3 {verbose = True}) mixed
+  return ()
