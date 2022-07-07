2b9edbf9f0a2e290ac147f97e34644a21b183dd0
Sirui Lu <lsrcz@outlook.com>
2022-07-07 11:22:36 -0700

    feat: microbenchmarks

---
+++ grisette-benchmarks/microsem/Unordered.hs
@@ -0,0 +1,29 @@
+module Unordered where
+
+import SymBool
+import Data.Bifunctor
+import Prog
+
+type Unordered a = [(SymBool, a)]
+
+mrgIfUnordered :: (Eq a) => SymBool -> Unordered a -> Unordered a -> Unordered a
+mrgIfUnordered cond [] r = fmap (first (ands (nots cond))) r
+mrgIfUnordered cond ((c,v):l) r = case findMergeable r [] of
+  (Nothing, collected) -> (ands cond c, v) : mrgIfUnordered cond l collected
+  (Just (rc, _), collected) -> (ites cond c rc, v) : mrgIfUnordered cond l collected
+  where
+    findMergeable [] collected = (Nothing, collected)
+    findMergeable (rt@(_,rv):rl) collected
+      | v == rv = (Just rt, rl ++ collected)
+      | otherwise = findMergeable rl (rt:collected)
+
+allSymBoolUnordered :: Unordered a -> [SymBool]
+allSymBoolUnordered = fmap fst
+
+interpretProgUnordered :: Eq a => Prog a -> Unordered a
+interpretProgUnordered (Terminal v) = [(Con True, v)]
+interpretProgUnordered (If cond l r) =
+  mrgIfUnordered cond (interpretProgUnordered l) (interpretProgUnordered r)
+
+benchmarkUnordered :: Eq a => Prog a -> Int
+benchmarkUnordered = length . allSubSymBools . allSymBoolUnordered . interpretProgUnordered
